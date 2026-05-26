2465a675242ec55328d274ae0ea5e5efc3fd2edf
Subhranil123-ops <mandalsubhranil847@gmail.com>
2026-05-26 10:29:09 +0530

    added error and loading states

--- client/src/components/Compiler.jsx
+++ client/src/components/Compiler.jsx
@@ -1,3 +1,4 @@
+"use client"
 import Button from "../components/Button.jsx"
 import {
     Card,
@@ -10,7 +11,10 @@ import {
     FieldLabel,
 } from "@/components/ui/field"
 import { Textarea } from "@/components/ui/textarea"
+import { Skeleton } from "@/components/ui/skeleton"
+import { Toaster } from "@/components/ui/sonner"
 
+import { toast } from "sonner"
 
 import ResultCard from "../components/ResultCard.jsx";
 import { useState } from "react";
@@ -18,25 +22,27 @@ import axios from "axios";
 
 export default function Compiler({ placeholder, name, buttonLabel, label }) {
     const [input, setInput] = useState("");
-    const [result, setResult] = useState({
+    const [result, setResult] = useState({});
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
 
-    })
     const handleSubmit = async (e) => {
-
         e.preventDefault();
-
         try {
+            setLoading(true);
+            setError("");
             const res = await axios.post(`http://localhost:3000/${name}`, {
                 [name]: input
             });
             setResult(res.data);
         } catch (err) {
-            console.log(err);
+            setError(err.response.data?.message);
+            toast.error(err.response.data?.message, { duration: 3000 });
+            setResult({});
+        } finally {
+            setLoading(false);
         }
-
-
     }
-
 
     const LABELS = {
         result: "Result",
@@ -52,40 +58,58 @@ export default function Compiler({ placeholder, name, buttonLabel, label }) {
     }
 
     return (
-        <div className="mx-auto max-w-3xl w-full p-6">
-            <form className="flex flex-col justify-center items-center gap-4 " onSubmit={handleSubmit}>
-                <Field>
-                    <FieldLabel htmlFor="textarea-code" className="text-white fw-bold text-base">{label}</FieldLabel>
-                    <Textarea id="textarea-code" name={name}
-                       className="text-white resize-none overflow-y-auto h-64 scrollbar-thin scrollbar-thumb-gray-600 scrollbar-track-black"
-                        rows={10}
-                        placeholder={placeholder}
-                        value={input}
-                        onChange={(e) => setInput(e.target.value)}
-                        required />
-                    <Button buttonLabel={buttonLabel} />
-                </Field>
-                <div>
+        <><Toaster richColors closeButton position="top-center" />
 
-                </div>
+            <div className="mx-auto max-w-3xl w-full p-6">
+                <form className="flex flex-col justify-center items-center gap-4 " onSubmit={handleSubmit}>
+                    <Field>
+                        <FieldLabel htmlFor="textarea-code" className="text-white fw-bold text-base">{label}</FieldLabel>
+                        <Textarea id="textarea-code" name={name}
+                            className={`text-white resize-none overflow-y-auto h-64 scrollbar-thin scrollbar-thumb-gray-600 scrollbar-track-black ${error.length > 0 ? "border-red-500 focus-visible:ring-red-500" : ""} ${Object.keys(result).length > 0 ? "border-green-500 focus-visible:ring-green-500" : ""}`}
+                            rows={10}
+                            placeholder={placeholder}
+                            value={input}
+                            onChange={(e) => { setInput(e.target.value); setResult({}); setError(""); toast.dismiss(); }}
+                            required />
+                        <Button buttonLabel={buttonLabel}
+                            variant="outline"
+                        />
+                    </Field>
+                    <div>
 
-            </form>
+                    </div>
 
-            {
-                Object.keys(result).length > 0 && (
-                    <Card className="flex flex-col justify-center max-w-4xl mx-auto bg-zinc-900 border border-zinc-800 rounded-2xl">
-                        <CardDescription className="mb-6 text-center font-bold text-3xl text-white" >Expression Result</CardDescription>
-                        <CardContent className="grid grid-cols-2 gap-4">
-                           
-                            {
-                                Object.entries(result).map(([key, value]) => {
-                                    return <ResultCard key={key} title={LABELS[key]} value={value} />
-                                })
-                            }
-                        </CardContent>
-                    </Card>
-                )
-            }
-        </div>
+                </form>
+
+
+                loading && ?(<Card className="flex flex-col justify-center max-w-4xl mx-auto bg-zinc-900 border border-zinc-800 rounded-2xl">
+                    <CardDescription className="mb-6 text-center font-bold text-3xl text-white" >Expression Result</CardDescription>
+                    <CardContent className="grid grid-cols-2 gap-4">
+
+                        <Skeleton className="h-4 w-[250px]" />
+                        <Skeleton className="h-4 w-[250px]" />
+                        <Skeleton className="h-4 w-[250px]" />
+                        <Skeleton className="h-4 w-[250px]" />
+                    </CardContent>
+                </Card>):({
+                    Object.keys(result).length > 0 && (
+                        <Card className="flex flex-col justify-center max-w-4xl mx-auto bg-zinc-900 border border-zinc-800 rounded-2xl">
+                            <CardDescription className="mb-6 text-center font-bold text-3xl text-white" >Expression Result</CardDescription>
+                            <CardContent className="grid grid-cols-2 gap-4">
+
+                                {
+                                    Object.entries(result).map(([key, value]) => {
+                                        return <ResultCard key={key} title={LABELS[key]} value={value} />
+                                    })
+                                }
+                            </CardContent>
+                        </Card>
+                    )
+                })
+
+
+
+            </div>
+        </>
     )
 }
--- server/controller/home.js
+++ server/controller/home.js
@@ -4,9 +4,9 @@ module.exports.postExpressionForm = async (req, res) => {
     const { expression } = req.body;
     try {
         const result = await axios.post('http://localhost:5000/expression', { expression });
-        console.log(result.data);
-        res.json(result.data);
+        res.json(result?.data);
     } catch (err) {
+        const code = err.response?.status || 500;
         console.log(err.response.data?.message || "Something went wrong");
         res.status(code).json({ message: err.response.data?.message || "Something went wrong" });
     }
@@ -15,12 +15,11 @@ module.exports.postExpressionForm = async (req, res) => {
 
 module.exports.postAnalyzeForm = async (req, res) => {
     const { code } = req.body;
-    console.log(code);
     try {
         const result = await axios.post('http://localhost:5000/analyze', { code });
-        console.log(result.data);
         res.json(result.data);
     } catch (err) {
+        const code = err.response?.status || 500;
         console.log(err.response.data?.message || "Something went wrong");
         res.status(code).json({ message: err.response.data?.message || "Something went wrong" });
     }
