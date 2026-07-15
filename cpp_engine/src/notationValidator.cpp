bd3227a1865839e41e7d9eab2aa944bde992ae90
Subhranil123-ops <mandalsubhranil847@gmail.com>
2026-07-15 18:29:30 +0530

    Improved the Infix Validator

--- cpp_engine/src/notationValidator.cpp
+++ cpp_engine/src/notationValidator.cpp
@@ -18,27 +18,36 @@ void validateEnding(const string &exp)
         throw invalid_argument("Infix expression cannot end with an operator");
 }
 
-void validateTwoOperatorsAdjacent(const string &exp)
+char previousNonSpace(const string &exp, int i)
 {
-    int n = exp.size();
-    int check0 = 0;
-    for (int i = 0; i < n; ++i)
+    char prev = '\0';
+    int j = i - 1;
+    while (j < exp.size())
     {
-        char ch = exp[i];
-        if (ch == ' ')
-            continue;
-        if (isOperator(ch))
+        if (exp[j] != ' ')
         {
-            if (check0 == 1)
-                throw invalid_argument("Two operators can't be adjacent");
-
-            check0 = 1;
+            prev = exp[j];
+            break;
         }
-        else
-        {
-            check0 = 0;
-        }
+        j--;
     }
+    return prev;
+}
+
+char nextNonSpace(const string &exp, int i)
+{
+    char next = '\0';
+    int j = i + 1;
+    while (j >= 0)
+    {
+        if (exp[j] != ' ')
+        {
+            next = exp[j];
+            break;
+        }
+        j++;
+    }
+    return next;
 }
 
 void validateInfix(const string &exp)
@@ -49,11 +58,94 @@ void validateInfix(const string &exp)
     // validate ending
     validateEnding(exp);
 
-    // validate adjacent operators
-    validateTwoOperatorsAdjacent(exp);
+    // traversal through the expression
+    int n = exp.size();
+    for (int i = 0; i < n; ++i)
+    {
+        if (exp[i] == ' ')
+            continue;
+        char ch = exp[i];
+        
+        // prev of curr character
+        char prev = previousNonSpace(exp, i);
+        
+        // for multidigit numbers
+        if (isdigit(ch))
+        {
+            while (i + 1 < n && isdigit(exp[i + 1]))
+                i++;
+        }
+        
+        //next of curr character
+        char next = nextNonSpace(exp, i);
+
+        // Starting
+        if (prev == '\0' && isOperator(ch))
+        {
+            throw invalid_argument("Infix expression cannot start with an operator");
+        }
+
+        // Ending
+        if (next == '\0' && isOperator(ch))
+        {
+            throw invalid_argument("Infix expression cannot end with an operator");
+        }
+        
+        
+        if (isOperator(ch))
+        {
+            if (isOperator(prev) || isOperator(next))
+            {
+                throw invalid_argument("Two operators cannot be adjacent");
+            }
+
+            if (prev == '(' || next == ')')
+            {
+                throw invalid_argument("Operator is missing an operand");
+            }
+        }
+        else if (ch == '(')
+        {
+            if (isdigit(prev) || prev == ')')
+            {
+                throw invalid_argument("Missing operator before '('");
+            }
+
+            if (isOperator(next) || next == ')')
+            {
+                throw invalid_argument("Invalid expression after '('");
+            }
+        }
+        else if (ch == ')')
+        {
+            if (isOperator(prev) || prev == '(')
+            {
+                throw invalid_argument("Invalid expression before ')'");
+            }
+
+            if (isdigit(next) || next == '(')
+            {
+                throw invalid_argument("Missing operator after ')'");
+            }
+        }
+        else if (isdigit(ch))
+        {
+            if (prev == ')')
+            {
+                throw invalid_argument("Missing operator before number");
+            }
+
+            if (next == '(')
+            {
+                throw invalid_argument("Missing operator before '('");
+            }
+
+            if (isdigit(next))
+                throw invalid_argument("Missing operator between operands");
+        }
+    }
 }
 
 void validatePostfix()
 {
-    
 }
